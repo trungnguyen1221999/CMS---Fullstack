082dda4cf8684be2d5d981d6c3cc597f67561f1d
Trung Nguyen <trungnguyen1221999@gmail.com>
2026-05-18 11:02:36 +0300

    seeding Identity

---
+++ BlogProject/src/Data/Seeding/IdentitySeeding.cs
@@ -0,0 +1,59 @@
+﻿using BlogProject.Core.Domain.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlogProject.Data.Seeding
+{
+    public class IdentitySeeding
+    {
+        public async Task Seeding(BlogContext context)
+        {
+            var passwordHasher = new PasswordHasher<AppUser>();
+
+            var seedingAdminRoleId = Guid.NewGuid();
+
+            if (!context.Roles.Any())
+            {
+                var seedingAdminRole = new AppRole
+                {
+                    Id = seedingAdminRoleId,
+                    Name = "Admin",
+                    NormalizedName = "ADMIN",
+                    DisplayName = "Administrator",
+                };
+                await context.Roles.AddAsync(seedingAdminRole);
+                await context.SaveChangesAsync();
+            }
+
+            if (!context.Users.Any())
+            {
+                var seedingAdminUser = new AppUser
+                {
+                    Id = Guid.NewGuid(),
+                    UserName = "kai_nguyen",
+                    NormalizedUserName = "KAI_NGUYEN",
+                    Email = "trungnguyen1221999@gmail.com",
+                    NormalizedEmail = "TRUNGNGUYEN1221999@GMAIL.COM",
+                    FirstName = "Kai",
+                    LastName = "Nguyen",
+                    IsActive = true,
+                    DateCreated = DateTime.UtcNow,
+                    ConcurrencyStamp = Guid.NewGuid().ToString(),
+                    LockoutEnabled = false,
+                };
+                seedingAdminUser.PasswordHash = passwordHasher.HashPassword(
+                    seedingAdminUser,
+                    "123456"
+                );
+                await context.Users.AddAsync(seedingAdminUser);
+                await context.UserRoles.AddAsync(
+                    new IdentityUserRole<Guid>
+                    {
+                        RoleId = seedingAdminRoleId,
+                        UserId = seedingAdminUser.Id,
+                    }
+                );
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
